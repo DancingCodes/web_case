84a95cb03f36c160a62e93cb15cd459c2b95539c
张长悦 <dancingcodes@qq.com>
2023-11-16 17:08:55 +0800

    图片复制上传

--- src/views/copyImgUpload/index.vue
+++ src/views/copyImgUpload/index.vue
@@ -1,10 +1,11 @@
 <template>
     <el-upload class="avatar-uploader" action="http://ff.topcrm.vip/prod-api/ff/common/upload" :show-file-list="false"
-        :on-success="handleAvatarSuccess">
+        :on-success="handleAvatarSuccess" @mouseenter.native="pasteInput.focus()">
         <img v-if="imageUrl" :src="imageUrl" class="avatar" />
         <el-icon v-else class="avatar-uploader-icon">
             <i-ep-Plus />
         </el-icon>
+        <input type="text" ref="pasteInput" @paste="getPasteValue" class="none">
     </el-upload>
 </template>
   
@@ -12,12 +13,24 @@
 import { ref } from 'vue'
 import type { UploadProps } from 'element-plus'
 const imageUrl = ref()
+const pasteInput = ref()
 const handleAvatarSuccess: UploadProps['onSuccess'] = (
     _response,
     uploadFile
 ) => {
+    // 在此处调用上传接口然后放入图片列表
+    // 模拟：使用图片的本地路径展示
     imageUrl.value = URL.createObjectURL(uploadFile.raw!)
 }
+
+const getPasteValue = (value: any) => {
+    const imageBlob = value.clipboardData.files[0]
+    // 在此处调用上传接口然后放入图片列表
+    // 模拟：使用图片的本地路径展示
+    console.log(URL.createObjectURL(imageBlob!))
+    imageUrl.value = URL.createObjectURL(imageBlob!)
+}
+
 </script>
   
 <style scoped lang="scss">
@@ -51,6 +64,12 @@ const handleAvatarSuccess: UploadProps['onSuccess'] = (
         border-color: var(--el-color-primary);
     }
 
+    .none {
+        width: 0;
+        height: 0;
+        padding: 0;
+        border: 0;
+    }
 
 }
 </style>
