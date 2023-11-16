d73f36ec6452467c84192df2a538c6e80baa8849
张长悦 <dancingcodes@qq.com>
2023-11-16 17:40:18 +0800

    switch 完善

--- src/views/switch/index.vue
+++ src/views/switch/index.vue
@@ -1,5 +1,8 @@
 <template>
     <h2 style="text-align: center;">switch</h2>
+
+    <div class="switch">
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -7,4 +10,11 @@
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.switch {
+    width: 100px;
+    height: 40px;
+    border: 1px solid var(--el-border-color);
+    border-radius: 40px;
+}
+</style>
